8d1f0bec981c4b92158714cceb4ceff36fe717cc
Ravi Ranjan <uranium@Ravis-Mac.local>
2015-07-29 15:52:57 +0530

    Initial Commit

---
+++ SimpleJSONTests/SimpleJSONTests.swift
@@ -0,0 +1,35 @@
+//
+//  SimpleJSONTests.swift
+//  SimpleJSONTests
+//
+//  Created by Ravi Ranjan on 29/07/15.
+//  Copyright © 2015 Ravi Ranjan. All rights reserved.
+//
+
+import XCTest
+
+class SimpleJSONTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
